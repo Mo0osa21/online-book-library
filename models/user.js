a5d5fba87a7467a845728585958e77fea47f15f4
nooraljohar <nooraljohar.a72@gmail.com>
2024-12-05 12:56:36 +0300

    noor last work

--- models/user.js
+++ models/user.js
@@ -9,7 +9,11 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true
-    }
+    },
+    isAdmin: { type: Boolean, default: false },
+    photo: { type: String, default: '' }
+  },
+  {
   },
   {
     timestamps: true
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
 const session = require('express-session')
+const multer = require('multer');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const passUserToView = require('./middleware/pass-user-to-view.js')
 const methodOverride = require('method-override')
 const authCtrl = require('./controllers/auth')
@@ -42,6 +46,50 @@ app.get('/', (req, res) => {
 app.get('/vip', isSignedIn, (req, res) => {
   res.send(`Welcome to the party ${req.session.user.username}.`)
 })
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up Multer storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Folder where files will be stored
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Rename the file to avoid duplicates
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const uploadDir = './uploads';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+app.use('/uploads', express.static('uploads'));
+/////////////////////////////////////////////////////////////////////////
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+// app.use(morgan('dev'));
+
+//new code below this line ----
+app.use(express.static(path.join(__dirname, 'public')));
+//new code above this line ---
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000')
