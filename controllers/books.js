f02bfba61ef3079a8cbd4d4be5f854cf44ecadc5
Moosa <moosajamel.21@gmail.com>
2024-12-09 12:22:00 +0300

    edit functionality for pdf an photo done

--- controllers/books.js
+++ controllers/books.js
@@ -3,8 +3,7 @@ const User = require('../models/user')
 const Review = require('../models/review')
 const Book = require('../models/book')
 const isAdmin = require('../middleware/is-admin')
-const upload = require('../middleware/upload');
-
+const upload = require('../middleware/upload')
 
 router.get('/', async (req, res) => {
   try {
@@ -22,44 +21,46 @@ router.get('/new', isAdmin, async (req, res) => {
   res.render('books/new.ejs')
 })
 
+router.post(
+  '/',
+  upload.fields([{ name: 'photo' }, { name: 'pdfFile' }]),
+  async (req, res) => {
+    try {
+      const { title, author, description, genre, isAvailable } = req.body
 
+      const newBook = new Book({
+        title,
+        author,
+        description,
+        genre,
+        isAvailable: isAvailable === 'on',
+        photo: req.files.photo[0].path,
+        pdfFile: req.files.pdfFile[0].path
+      })
 
+      await newBook.save()
 
-router.post('/',upload.fields([{ name: 'photo' }, { name: 'pdfFile' }]), async (req, res) => {
-
-  try {
-    const { title, author, description, genre, isAvailable } = req.body
-
-    const newBook = new Book({
-      title,
-      author,
-      description,
-      genre,
-      isAvailable: isAvailable === 'on',
-      photo: req.files.photo[0].path, 
-      pdfFile: req.files.pdfFile[0].path, 
-    })
-
-    await newBook.save()
-
-    res.redirect('/books')
-  } catch (error) {
-    console.error('Error adding book:', error)
-    res.status(500).send('An error occurred while adding the book.')
+      res.redirect('/books')
+    } catch (error) {
+      console.error('Error adding book:', error)
+      res.status(500).send('An error occurred while adding the book.')
+    }
   }
-})
-
+)
 
 router.get('/:bookId', async (req, res) => {
   try {
-    const book = await Book.findById(req.params.bookId);
-    const reviews = await Review.find({ book: book._id }).populate('user', 'username');
-    res.render('books/show.ejs', { book, reviews });
+    const book = await Book.findById(req.params.bookId)
+    const reviews = await Review.find({ book: book._id }).populate(
+      'user',
+      'username'
+    )
+    res.render('books/show.ejs', { book, reviews })
   } catch (error) {
-    console.log(error);
-    res.redirect('/');
+    console.log(error)
+    res.redirect('/')
   }
-});
+})
 
 router.get('/:bookId/edit', isAdmin, async (req, res) => {
   try {
@@ -73,36 +74,48 @@ router.get('/:bookId/edit', isAdmin, async (req, res) => {
   }
 })
 
-router.put('/:bookId', async (req, res) => {
-  try {
-    const currentBook = await Book.findById(req.params.bookId);
+router.put(
+  '/:bookId',
+  upload.fields([
+    { name: 'photo', maxCount: 1 },
+    { name: 'pdfFile', maxCount: 1 }
+  ]),
+  async (req, res) => {
+    try {
+      const updatedInfo = {
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre
+      }
+      if (req.body.isAvailable !== undefined) {
+        updatedInfo.isAvailable = req.body.isAvailable === 'on'
+      } else {
+        updatedInfo.isAvailable = false
+      }
 
-    if (!currentBook) {
-      return res.status(404).send('Book not found');
+      if (req.files && req.files.photo) {
+        updatedInfo.photo = req.files.photo[0].path
+      }
+
+      if (req.files && req.files.pdfFile) {
+        updatedInfo.pdfFile = req.files.pdfFile[0].path
+      }
+
+      await Book.findByIdAndUpdate(req.params.bookId, updatedInfo)
+
+      res.redirect(`/books/${req.params.bookId}`)
+    } catch (error) {
+      console.log(error)
+      res.redirect(`/books/${req.params.bookId}/edit`)
     }
-
-    if (req.body.title !== undefined) currentBook.title = req.body.title;
-    if (req.body.author !== undefined) currentBook.author = req.body.author;
-    if (req.body.isAvailable !== undefined) {
-      currentBook.isAvailable = req.body.isAvailable === 'on'; 
-    }else{
-      currentBook.isAvailable = false;
-    }
-
-    await currentBook.save();
-
-    res.redirect('/books');
-  } catch (error) {
-    console.error(error);
-    res.redirect('/');
   }
-});
+)
 
 router.delete('/:bookId', isAdmin, async (req, res) => {
   try {
     const book = await Book.findById(req.params.bookId)
-      await Book.deleteOne()
-      res.redirect('/books')
+    await Book.deleteOne()
+    res.redirect('/books')
   } catch (error) {
     console.error(error)
     res.redirect('/')
